b944a9bf7dcf8f02f8c73fdf9f3ae3a1c6aeae60
Harshit Lakhawat <110206harshit@gmail.com>
2024-10-20 16:40:05 +0530

    Update peak element in mountain array.cpp

--- peak element in mountain array.cpp
+++ peak element in mountain array.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 
-  int peakindexinmountainarray(vector<int>&arr){
+  /*int peakindexinmountainarray(vector<int>& arr){
     int s = 0;
     int  e = arr.size() - 1;
 
@@ -17,11 +17,33 @@ using namespace std;
         e = mid;
       }
       mid = s + (e-s)/2;
-      
+
     }
     return s;
-    
+
+  }*/
+int peakindexinmountainarray(vector<int>& arr){
+  int s = 0;
+  int  e = arr.size() - 1;
+
+  int mid = s + (e-s)/2;
+
+  while(s < e){
+    if(arr[mid] > arr[mid+1] && arr[mid] > arr[mid-1]){ // Check both neighbors
+      return mid; // Found the peak, return the index
+    }
+    else if(arr[mid] < arr[mid+1]){
+      s = mid + 1;
+    }
+    else{
+      e = mid;
+    }
+    mid = s + (e-s)/2; // Recalculate mid AFTER updating s or e
+
   }
+  return s; // If the loop ends without finding a peak, return s (which will be the index of the last element)
+
+}
 int main(){
   vector<int> arr = {1,2,3,4,5,6,4,3,2,1,0};
   int peakindex = peakindexinmountainarray(arr);
