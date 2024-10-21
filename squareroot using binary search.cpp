65e2e64616dab02d506a819e7096e8003902a8f6
Harshit Lakhawat <110206harshit@gmail.com>
2024-10-21 10:56:56 +0530

    Create squareroot using binary search.cpp

---
+++ squareroot using binary search.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int binarysearch(int n){
+
+  int s = 0;
+  int e = n;
+  int mid = s + (e-s)/2 ;
+
+  int ans = -1;
+  while (s <= e){
+    int square = mid*mid;
+
+    if(square == n){
+      return mid;
+    }
+    if(square < n){
+      ans = mid;
+      s = mid - 1;
+    }
+    else{
+      e = mid - 1;
+    }
+    mid = s + (e-s)/2;
+  }
+  return ans;
+}
+
+int main(){
+  int n ;
+  cout << "write no. whose squareroot you want to know : " ;
+  cin >> n;
+
+  cout << "the square root is : " << binarysearch(n) << endl;
+}
