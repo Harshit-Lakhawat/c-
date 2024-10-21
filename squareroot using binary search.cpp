b8d97661a0d65d477c0ebcca1aa5a42e6dd286ee
Harshit Lakhawat <110206harshit@gmail.com>
2024-10-21 11:32:32 +0530

    Update squareroot using binary search.cpp

--- squareroot using binary search.cpp
+++ squareroot using binary search.cpp
@@ -1,22 +1,22 @@
 #include<iostream>
 using namespace std;
 
-int binarysearch(int n){
+long long int sqrtinteger(int n){
 
   int s = 0;
   int e = n;
-  int mid = s + (e-s)/2 ;
+  long long int mid = s + (e-s)/2 ;
 
-  int ans = -1;
+  long long int ans = -1;
   while (s <= e){
-    int square = mid*mid;
+    long long int square = mid*mid;
 
     if(square == n){
       return mid;
     }
     if(square < n){
       ans = mid;
-      s = mid - 1;
+      s = mid + 1;
     }
     else{
       e = mid - 1;
@@ -26,10 +26,26 @@ int binarysearch(int n){
   return ans;
 }
 
+double moreprecision(int n, int precision, int tempsol){
+  double factor = 1.0;
+  double ans = tempsol;
+
+  for(int i=0; i<precision; i++){
+    factor = factor/10;
+
+    for(double j=ans; j*j<n; j=j+factor){
+      ans = j;
+    }
+  }
+  return ans;
+}
+
 int main(){
   int n ;
   cout << "write no. whose squareroot you want to know : " ;
   cin >> n;
 
-  cout << "the square root is : " << binarysearch(n) << endl;
+  int tempsol = sqrtinteger(n);
+  cout << "answer is : "<< moreprecision(n, 3, tempsol);
+  return 0;
 }
