744f00205e02a91ce16e07246742b3ff644998f6
Harshit Lakhawat <110206harshit@gmail.com>
2024-10-22 16:33:55 +0530

    Create insertion sort.cpp

---
+++ insertion sort.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void insertionsort(int n, int arr[]){
+
+  for(int i = 1; i<n; i++){
+    int temp = arr[i];
+    int j = i-1;
+    for(; j>=0; j--){
+
+      if(arr[j] > temp){
+        arr[j+1] = arr[j];
+      }
+      else{
+        break;
+      }
+    }
+    arr[j+1] = temp;
+  }
+}
