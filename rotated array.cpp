5071c52faabaf8b1d22be4f83e5a85ab44d2c4f9
Harshit Lakhawat <110206harshit@gmail.com>
2024-10-26 00:12:06 +0530

    Create rotated array.cpp

---
+++ rotated array.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void rotate(vector<int> &nums, int k) {
+
+  vector<int> temp(nums.size());
+
+  for (int i = 0; i < nums.size(); i++) {
+    temp[(i + k) % nums.size()] = nums[i];
+  }
+
+  nums = temp;
+}
+
+int main() { 
+  
+  vector<int> nums = {1,2,3,4,5,6,7,8};
+  int k = 3 ;
+  rotate(nums, k);
+
+  for (int i = 0; i < nums.size() ; i++){
+    cout<<nums[i]<<" ";
+  }
+           
+}
