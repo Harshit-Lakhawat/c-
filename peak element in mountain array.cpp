0f6af68412c49d25aa45886331cb9700b8b6b736
Harshit Lakhawat <110206harshit@gmail.com>
2024-12-21 13:23:39 +0530

    Update peak element in mountain array.cpp

--- peak element in mountain array.cpp
+++ peak element in mountain array.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 
-  /*int peakindexinmountainarray(vector<int>& arr){
+  int peakindexinmountainarray(vector<int>& arr){
     int s = 0;
     int  e = arr.size() - 1;
 
@@ -21,69 +21,10 @@ using namespace std;
     }
     return s;
 
-  }*/
-int peakindexinmountainarray(vector<int>& arr){
-  int s = 0;
-  int  e = arr.size() - 1;
-
-  int mid = s + (e-s)/2;
-
-  while(s < e){
-    if(arr[mid] > arr[mid+1] && arr[mid] > arr[mid-1]){ // Check both neighbors
-      return mid; // Found the peak, return the index
-    }
-    else if(arr[mid] < arr[mid+1]){
-      s = mid + 1;
-    }
-    else{
-      e = mid;
-    }
-    mid = s + (e-s)/2; // Recalculate mid AFTER updating s or e
-
-  }
-  return s; // If the loop ends without finding a peak, return s (which will be the index of the last element)
-
-}
-int main(){
-  vector<int> arr = {1,2,3,4,5,6,4,3,2,1,0};
-  int peakindex = peakindexinmountainarray(arr);
-  cout << "peak index is " << peakindex << endl;
-  return 0;
-}
-
-
-//corrected code :
-
-
-/*#include<iostream>
-#include<vector>
-using namespace std;
-
-int peakindexinmountainarray(vector<int>&arr){
-    int s = 0;
-    int  e = arr.size() - 1;
-
-    int mid = s + (e-s)/2;
-
-    while(s < e){
-      if(arr[mid] > arr[mid+1] && arr[mid] > arr[mid-1]){ // Check both neighbors
-        return mid; // Found the peak, return the index
-      }
-      else if(arr[mid] < arr[mid+1]){
-        s = mid + 1;
-      }
-      else{
-        e = mid;
-      }
-      mid = s + (e-s)/2;
-
-    }
-    return s; // If the loop ends without finding a peak, return s (which will be the index of the last element)
-
   }
 int main(){
   vector<int> arr = {1,2,3,4,5,6,4,3,2,1,0};
   int peakindex = peakindexinmountainarray(arr);
   cout << "peak index is " << peakindex << endl;
   return 0;
-}*/
+}
